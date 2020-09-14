38cbcff7e4569e0f7ef28aba2c07e9fcb8a8652e
antoniogambogi <antoniogambogi88@gmail.com>
2020-09-14 16:25:12 -0300

    Adicionando song-card

--- src/app/modules/songs/songs.module.ts
+++ src/app/modules/songs/songs.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { SongsRoutingModule } from './songs-routing.module';
 import { SongsComponent } from './songs.component';
+import { SongCardComponent } from './song-card/song-card.component';
 
 
 @NgModule({
-  declarations: [SongsComponent],
+  declarations: [
+    SongsComponent,
+    SongCardComponent
+  ],
   imports: [
     CommonModule,
     SongsRoutingModule,
-    MatCardModule
+    MatCardModule,
+    FlexLayoutModule
   ]
 })
 export class SongsModule { }
