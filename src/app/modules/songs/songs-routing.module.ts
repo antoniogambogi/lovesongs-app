99f67c1f2a7bb31db59116c45c4fe1f7e46bdfaa
antoniogambogi <antoniogambogi88@gmail.com>
2020-09-14 16:47:08 -0300

    Criando o componente song-detail

--- src/app/modules/songs/songs-routing.module.ts
+++ src/app/modules/songs/songs-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SongDetailComponent } from './song-detail/song-detail.component';
 
 import { SongsComponent } from './songs.component'
 
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: SongsComponent
+  },
+  {
+    path: 'detail/:songName',
+    component: SongDetailComponent
   }
 ];
 
--- src/app/modules/songs/songs.module.ts
+++ src/app/modules/songs/songs.module.ts
@@ -7,12 +7,14 @@ import { FlexLayoutModule } from '@angular/flex-layout'
 import { SongsRoutingModule } from './songs-routing.module';
 import { SongsComponent } from './songs.component';
 import { SongCardComponent } from './song-card/song-card.component';
+import { SongDetailComponent } from './song-detail/song-detail.component';
 
 
 @NgModule({
   declarations: [
     SongsComponent,
-    SongCardComponent
+    SongCardComponent,
+    SongDetailComponent
   ],
   imports: [
     CommonModule,
