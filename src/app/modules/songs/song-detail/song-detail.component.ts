7ca21c0dd7331a781451234d3fed3d87b591ff23
antoniogambogi <antoniogambogi88@gmail.com>
2020-09-14 17:13:01 -0300

    Implementando navegação entre songs e song detail

--- src/app/modules/songs/song-detail/song-detail.component.ts
+++ src/app/modules/songs/song-detail/song-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-song-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const songName = this.activatedRoute.snapshot.params['songName']
   }
 
 }
